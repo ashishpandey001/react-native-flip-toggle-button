635d349951f106ce6bcbd19974d1e818a196cb0e
Ashish Pandey <hmaster0@gmail.com>
2017-12-01 16:22:30 +0530

    add an example app

--- FlipToggleSample/App.js
+++ FlipToggleSample/App.js
@@ -1,37 +1,100 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { StyleSheet, Text, View } from 'react-native';
+import FlipToggle from 'react-native-flip-toggle-button';
 
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={styles.item}>
+          <Text>Regular toggle button</Text>
+          <FlipToggle
+            buttonWidth={50}
+            buttonHeight={25}
+            onToggle={() => {
+              console.log();
+            }}
+          />
+        </View>
+        <View style={styles.item}>
+          <Text>Rounded toggle button</Text>
+          <FlipToggle
+            buttonWidth={50}
+            buttonHeight={25}
+            buttonRadius={50}
+            onToggle={() => {
+              console.log();
+            }}
+          />
+        </View>
+        <View style={styles.item}>
+          <Text>Wide toggle button</Text>
+          <FlipToggle
+            buttonWidth={400}
+            buttonHeight={25}
+            onToggle={() => {
+              console.log();
+            }}
+          />
+        </View>
+        <View style={styles.item}>
+          <Text>Rounded Wide toggle button</Text>
+          <FlipToggle
+            buttonWidth={400}
+            buttonHeight={25}
+            buttonRadius={50}
+            onToggle={() => {
+              console.log();
+            }}
+          />
+        </View>
+        <View style={styles.item}>
+          <Text>
+            Toggle button with custom dimensions for toggle button and slider
+          </Text>
+          <FlipToggle
+            buttonWidth={200}
+            buttonHeight={50}
+            sliderWidth={20}
+            sliderHeight={30}
+            onToggle={() => {
+              console.log();
+            }}
+          />
+        </View>
+        <View style={styles.item}>
+          <Text>
+            Toggle button with custom on and off labels, on and off button and
+            slider colors
+          </Text>
+          <FlipToggle
+            buttonWidth={200}
+            buttonHeight={25}
+            buttonRadius={50}
+            buttonOffColor={'#666'}
+            sliderOffColor={'#000'}
+            buttonOnColor={'blue'}
+            sliderOnColor={'red'}
+            onLabel={'on'}
+            offLabel={'off'}
+            onToggle={() => {
+              console.log();
+            }}
+          />
+        </View>
+        <View style={styles.item}>
+          <Text>Big Fat toggle button</Text>
+          <FlipToggle
+            buttonWidth={400}
+            buttonHeight={150}
+            buttonRadius={50}
+            onLabel={'on'}
+            offLabel={'off'}
+            onToggle={() => {
+              console.log();
+            }}
+          />
+        </View>
       </View>
     );
   }
@@ -40,18 +103,9 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 10
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  item: {
+    marginVertical: 5
+  }
 });
