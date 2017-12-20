0c44e3a42d7a9cb9b1ac527b2ae6d4b33957c822
Ashish Pandey <ashishpandeyone@gmail.com>
2017-12-20 12:32:33 +0530

    Upgrade to rn 0.51 (#1)
    * upgrade sample app to use react-native 0.51.1
    
    * upgrade to use latest version of the package and fix runtime issue in sample app

--- FlipToggleSample/App.js
+++ FlipToggleSample/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import FlipToggle from './FlipToggle';
+import FlipToggle from 'react-native-flip-toggle-button';
 
 export default class App extends Component<{}> {
   render() {
